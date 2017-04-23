e1eb996bfad019ad17334635b9e58f41078b6d8c
Spirevipp <bjornar.helgeland@hotmail.com>
2017-04-23 18:08:43 +0200

    add stuff

---
+++ Test Project/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <NewPing.h>
+
+
+//Define analog pins
+#define IR_SENSOR A0
+#define LIGHT_SENSOR_ANALOG A1
+#define TEMP_SENSOR A2
+#define SOUND_SENSOR_ANALOG A3
+
+//Define digital pins
+#define LIGHT_SENSOR_DIGITAL 6
+#define SOUND_SENSOR_DIGITAL 7
+#define RANGE_SENSOR_ECHO 8
+#define RANGE_SENSOR_TRIG 9
+
+void setup()  {
+
+}
+
+void loop() {
+
+}
