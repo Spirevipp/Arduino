50ced1ca71352c2e3dbf3079d11a20d240bd7990
Spirevipp <bjornar.helgeland@hotmail.com>
2017-04-24 19:56:56 +0200

    bugfix

--- Libraries/SpireLib/examples/function usage.cpp
+++ Libraries/SpireLib/examples/function usage.cpp
@@ -2,7 +2,7 @@
 #include "SpireLib.h"
 #include "Servo.h"    // not actually required for using the library, just to demonstrate the servo function
 
-#define LEDPIN 13     // the onboard led connected to digital pin 13
+#define LEDPIN 11     // a led connected to digital pin 11
 
 Pot servoPot(0);      // The pin used is an analog input pin, so you can use A0-5 or just 0-5
 Pot pwmPot(1);        // The pin used is an analog input pin, so you can use A0-5 or just 0-5
@@ -21,16 +21,25 @@ void loop()   {
   //  transforms it to a range usable by the servo (0 - 180)
   //  then writes the value to the servo
   myServo.write(servoPot.readServo());
+  Serial.print("Servo: ");
+  Serial.print(servoPot.readServo());
+  Serial.print("  ");
 
-  //  This writes a PWM signal to the onboard led
+  //  This writes a PWM signal to the led
   //  using the value read from the potentiometer
   //  like the example above, the value has been
   //  transformed into a range, this time the PWM range (0 - 255)
   analogWrite(LEDPIN, pwmPot.readPWM());
+  Serial.print("PWM: ");
+  Serial.print(pwmPot.readPWM());
+  Serial.print("  ");
 
   //  This reads the temperature from the temperature sensor
   //  and logs it to the serial monitor
   Serial.print("The temperature is ");
   Serial.print(myTemp.readC());
-  Serial.println(" degrees celsius!");
+  Serial.print(" ");
+  Serial.print("degrees celsius!");
+
+Serial.println(" ");
 }
