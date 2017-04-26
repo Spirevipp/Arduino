9ba0e63b5625d5d578682d8b616e1745c5d57ba0
Spirevipp <bjornar.helgeland@hotmail.com>
2017-04-26 13:01:18 +0200

    robotic arm

---
+++ Projects/Robotic arm/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+Servo servo6;
+
+
+void setup()  {
+
+  servo1.attach(4);
+  servo2.attach(5);
+  servo3.attach(6);
+  servo4.attach(7);
+  servo5.attach(8);
+  servo6.attach(9);
+
+}
+
+void loop() {
+
+
+}
