9f4da332a04dfb8fa742f5615842287d778bdce7
Spirevipp <bjornar.helgeland@hotmail.com>
2017-05-08 14:53:06 +0200

    ghghhh

--- Projects/Stoppeklokke/src/main.cpp
+++ Projects/Stoppeklokke/src/main.cpp
@@ -9,6 +9,11 @@ unsigned long klokke = 0;
 unsigned long sek = 0;
 unsigned long min = 0;
 unsigned long mil = 0;
+unsigned long hr = 0;
+unsigned long psek = 0;
+unsigned long pmin = 0;
+unsigned long pmil = 0;
+unsigned long phr = 0;
 
 bool start = false;
 
@@ -25,6 +30,7 @@ void loop() {
                         sek = 0;
                         min = 0;
                         mil = 0;
+                        hr = 0;
                         start = true;
                         delay(100);
                 }
@@ -48,9 +54,19 @@ void loop() {
                 sek = 0;
                 min++;
         }
-        if (start)  {
+        if (min == 60) {
+                min = 0;
+                hr++;
+        }
+        if (start && (min != pmin || mil != pmil || sek != psek || hr != phr))  {
+                pmin = min;
+                psek = sek;
+                pmil = mil;
+                phr = hr;
                 lcd.clear();
                 lcd.setCursor(0, 0);
+                lcd.print(hr);
+                lcd.print(":");
                 lcd.print(min);
                 lcd.print(":");
                 lcd.print(sek);
