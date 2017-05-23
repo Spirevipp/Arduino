92b1a2a4d1a34204bb32374b9bf3fc13cf31b3f7
Spirevipp <bjornar.helgeland@hotmail.com>
2017-05-23 23:34:34 +0200

    gffg

--- Projects/NodeMCU Blink/src/main.cpp
+++ Projects/NodeMCU Blink/src/main.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 
-#define LED 13
+#define LED D7
+#define PENN D6
 
 int buttonpressed = 5;
 int firstrun = 0;
 
 unsigned long pMil = 0;
+bool enableBlink = false;
 
 char *ssid = "Laksen";
 char *password = "arxofgecrirzu";
@@ -15,6 +17,7 @@ WiFiServer server(80); // Define Web Server Port
 
 void setup() {
   pinMode(LED, OUTPUT);
+  pinMode(PENN, OUTPUT);
 
   Serial.begin(115200);
   delay(10);
@@ -45,6 +48,15 @@ void setup() {
   Serial.println();
 }
 void loop() {
+
+  // blink toggler
+  if (enableBlink) {
+    if (pMil + 250 <= millis()) {
+      digitalWrite(LED, !digitalRead(LED));
+      pMil = millis();
+    }
+  }
+
   // Check if someone is connected
   WiFiClient client = server.available();
   if (!client) {
@@ -55,20 +67,35 @@ void loop() {
   String request = client.readStringUntil('\r');
 
   // Check recieved buttonpress
-  if (request.indexOf("/ON=1") != -1) {
-    digitalWrite(LED, HIGH);
-    buttonpressed = HIGH;
-    firstrun = 1;
-  }
   if (request.indexOf("/OFF=1") != -1) {
     digitalWrite(LED, LOW);
-    buttonpressed = LOW;
+    enableBlink = false;
+    buttonpressed = 0;
+    firstrun = 1;
+  }
+  if (request.indexOf("/ON=1") != -1) {
+    digitalWrite(LED, HIGH);
+    enableBlink = false;
+    buttonpressed = 1;
+    firstrun = 1;
+  }
+  if (request.indexOf("/BLINK=1") != -1) {
+    enableBlink = !enableBlink;
+    buttonpressed = 2;
     firstrun = 1;
   }
   if (request.indexOf("/Rave=1") != -1) {
+    enableBlink = false;
+    buttonpressed = 0;
+    firstrun = 1;
     digitalWrite(LED, HIGH);
     delay(50);
     digitalWrite(LED, LOW);
+  }
+  if (request.indexOf("/Penn=1") != -1) {
+    digitalWrite(PENN, HIGH);
+    delay(50);
+    digitalWrite(PENN, LOW);
   }
 
   // Create Web Page
@@ -86,24 +113,35 @@ void loop() {
   // update webpage
 
   if (firstrun == 0) {
-    client.print("Please Click a Button ");
+    client.print("LED is currently OFF");
   } else {
     client.print("LED is ");
   }
 
-  if (buttonpressed == HIGH) {
+  if (buttonpressed == 0) {
+    client.print("OFF");
+  }
+  if (buttonpressed == 1) {
     client.print("ON");
   }
-  if (buttonpressed == LOW) {
-    client.print("OFF");
+  if (buttonpressed == 2) {
+    client.print("BLINKING");
   }
 
   client.println("<br><br>");
   client.println("<a href=\"/ON=1\"\"><button>ON </button></a>");
   client.println("<a href=\"/OFF=1\"\"><button>OFF</button></a><br />");
 
+  client.println("<br><br>");
   client.print("Hanne rave!");
   client.println("<a href=\"/Rave=1\"\"><button>RAVE</button></a>");
+
+  client.println("<br><br>");
+  client.println("<a href=\"/BLINK=1\"\"><button>BLINK</button></a>");
+
+  client.println("<br><br>");
+  client.print("Penn knapp");
+  client.println("<a href=\"/Penn=1\"\"><button>PENN</button></a>");
 
   client.println("</html>");
   delay(10);
