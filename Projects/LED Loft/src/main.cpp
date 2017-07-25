fbb9e50af647850662aa4870b323c762b39c4210
Spirevipp <bjornar.helgeland@hotmail.com>
2017-07-25 23:11:59 +0200

    More stuff to led loft

--- Projects/LED Loft/src/main.cpp
+++ Projects/LED Loft/src/main.cpp
@@ -1,5 +1,62 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
 
-void setup() {}
+// Defines
+#define firstPin 2
+#define lastPin 13
 
-void loop() {}
+#define buttonR 5
+#define buttonG 6
+#define buttonB 7
+#define buttonW 8
+
+#define displayPower 2
+
+// Function initializers
+void showDisplay();
+
+// Variables
+unsigned long displayTimer = 0;
+bool buttonPressed = false;
+
+void setup() {
+
+  for (int i = 0; i < 14; i++) {
+    pinMode(i, INPUT_PULLUP);
+  }
+  for (int i = firstPin; i < lastPin; i++) {
+    pinMode(i, OUTPUT);
+  }
+}
+
+void loop() {
+  if (digitalRead(buttonR) == LOW || digitalRead(buttonG) == LOW ||
+      digitalRead(buttonB) == LOW || digitalRead(buttonW) == LOW) {
+    buttonPressed = true;
+  }
+  showDisplay();
+}
+
+// Functions
+
+void showDisplay() {
+  bool isActive;
+  if (buttonPressed) {
+    displayTimer = millis();
+  }
+  if (millis() - displayTimer < 10000) {
+    digitalWrite(displayPower, HIGH);
+    isActive = true;
+
+  } else {
+    digitalWrite(displayPower, LOW);
+    isActive = false;
+  }
+  if (isActive) {
+    // Display Stuff
+
+    // Show ip
+    // current color
+    // Current brightness
+  }
+}
